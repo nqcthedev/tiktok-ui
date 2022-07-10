716b54dfe4070f7048ce83089d5cfb98a19860bf
nguyenquoccuong <nguyenquoccuong98x@gmail.com>
2022-07-10 15:12:34 +0700

    code UI navbar Search

---
+++ src/assets/images/index.js
@@ -0,0 +1,4 @@
+const image = {
+    logo: require('~/assets/images/logo.svg'),
+};
+export default image;
--- src/components/Layouts/components/Header/index.js
+++ src/components/Layouts/components/Header/index.js
@@ -1,15 +1,34 @@
+import { faCircleXmark, faSearch, faSpinner } from '@fortawesome/free-solid-svg-icons';
+
+//Icon
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React from 'react';
-import className from 'classnames/bind'
-import styles from './Header.module.scss'
-const cx = className.bind(styles)
+import className from 'classnames/bind';
+import images from '~/assets/images';
+import styles from './Header.module.scss';
+
+const cx = className.bind(styles);
 const Header = () => {
     return (
-       <header className={cx('wrapper')}>
-        <div className={cx('inner')}>
-            {/* Logo */}
-            {/* Search */}
-        </div>
-       </header>
+        <header className={cx('wrapper')}>
+            <div className={cx('inner')}>
+                <div className={cx('logo')}>
+                    <img src={images.logo.default} alt="logo" />
+                </div>
+                <div className={cx('search')}>
+                    <input placeholder="Search accounts and videos" spellCheck={false} />
+                    <button className={cx('clear')}>
+                        <FontAwesomeIcon icon={faCircleXmark} />
+                    </button>
+                    {/* Loading */}
+                    <FontAwesomeIcon className={cx('loading')} icon={faSpinner} />
+                    <button className={cx('search-btn')}>
+                        <FontAwesomeIcon icon={faSearch} />
+                    </button>
+                </div>
+                <div className={cx('action')}>123</div>
+            </div>
+        </header>
     );
 };
 
