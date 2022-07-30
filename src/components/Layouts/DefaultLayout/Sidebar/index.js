fc6c0ad40acdbef28bd88952371d6b835e52cbb8
Cường Nguyễn Quốc <nguyenquoccuong1998x@gmail.com>
2022-07-30 09:11:20 +0700

    update actios

--- src/components/Layouts/DefaultLayout/Sidebar/index.js
+++ src/components/Layouts/DefaultLayout/Sidebar/index.js
@@ -1,15 +1,39 @@
-import React from 'react'
-import className from 'classnames/bind'
+import React from 'react';
+import className from 'classnames/bind';
 //Scss
-import styles from "./Sidebar.module.scss"
+import styles from './Sidebar.module.scss';
 
-const cx = className.bind(styles)
+const cx = className.bind(styles);
 const Sidebar = () => {
-  return (
-    <aside className={cx('wrapper')}>
-      
-    </aside>
-  )
-}
+    return (
+        <aside className={cx('wrapper')}>
+            <h1>Hello</h1>
+            <h1>Hello</h1>
+            <h1>Hello</h1>
+            <h1>Hello</h1>
+            <h1>Hello</h1>
+            <h1>Hello</h1>
+            <h1>Hello</h1>
+            <h1>Hello</h1>
+            <h1>Hello</h1>
+            <h1>Hello</h1>
+            <h1>Hello</h1>
+            <h1>Hello</h1>
+            <h1>Hello</h1>
+            <h1>Hello</h1>
+            <h1>Hello</h1>
+            <h1>Hello</h1>
+            <h1>Hello</h1>
+            <h1>Hello</h1>
+            <h1>Hello</h1>
+            <h1>Hello</h1>
+            <h1>Hello</h1>
+            <h1>Hello</h1>
+            <h1>Hello</h1>
+            <h1>Hello</h1>
+            <h1>Hello</h1>
+        </aside>
+    );
+};
 
-export default Sidebar
+export default Sidebar;
